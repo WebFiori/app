9a0eee3c31390421e1faa203ea40e5ce3ffd0f83
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2021-07-05 22:27:42 +0300

    Create APIRoutes.php

--- app/ini/routes/APIRoutes.php
+++ app/ini/routes/APIRoutes.php
@@ -3,22 +3,14 @@
 namespace app\ini\routes;
 
 use webfiori\framework\router\Router;
-/**
- * A class that only has one method to initiate some of system routes.
- * The class is meant to only initiate the routes which uses the method 
- * Router::api().
- * @author Ibrahim
- * @version 1.0
- */
+
 class APIRoutes {
     /**
-     * Create all API routes. Include your own here.
+     * Initialize system routes.
+     * 
      * @since 1.0
      */
     public static function create() {
-        Router::api([
-            'path' => '/ExampleAPI/{service-name}', 
-            'route-to' => '/ExampleAPI.php'
-        ]);
+        //TODO: Add your own routes here.
     }
 }
