bdac27169b59329d7d76567b232020dce586f4e7
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2021-09-21 21:16:45 +0300

    Update cron.js

--- public/assets/js/cron.js
+++ public/assets/js/cron.js
@@ -4,6 +4,17 @@ var app = new Vue({
     data: {
         password:'',
         loading:false,
+        jobs:[],
+        jobs_table_headers:[
+            {value:'name', text:'Job Name'},
+            {value:'expression', text:'CRON Expression'},
+            {value:'time.is_minute', text:'Is Minute'},
+            {value:'time.is_hour', text:'Is Hour'},
+            {value:'time.is_day_of_week', text:'Is Day of Week'},
+            {value:'time.is_day_of_month', text:'Is Day of Month'},
+            {value:'time.is_month', text:'Is Month'},
+            {value:'actions', text:'Actions'},
+        ],
         dialog:{
             show:false,
             message:''
@@ -15,7 +26,60 @@ var app = new Vue({
             return pass.trim().length === 0;
         }
     },
+    mounted:function () {
+        if (data.title === 'Scheduled CRON Tasks') {
+            this.loadTasks();
+        }
+    },
     methods:{
+        forceExec:function (job) {
+            console.log(job);
+        },
+        loadTasks:function() {
+            var vue = this;
+            new AJAXRequest({
+                method:'get',
+                url:'cron/apis/get-jobs',
+                beforeAjax:function() {
+                    vue.loading = true;
+                },
+                afterAjax:function() {
+                    vue.loading = false;
+                },
+                onSuccess:function() {
+                    if (this.jsonResponse) {
+                        vue.jobs = this.jsonResponse.jobs;
+                    } else {
+                        vue.showDialog('Something went wrong. Try again.');
+                    }
+                },
+                onServerErr:function() {
+                    if (this.jsonResponse) {
+                        if (this.jsonResponse.message) {
+                            vue.showDialog(this.jsonResponse.message);
+                        } else {
+                            vue.showDialog(this.status+' - Server Error.');
+                        }
+                    } else {
+                        vue.showDialog(this.status+' - Server Error.');
+                    }
+                },
+                onClientErr:function() {
+                    if (this.jsonResponse) {
+                        if (this.jsonResponse.message) {
+                            vue.showDialog(this.jsonResponse.message);
+                        } else {
+                            vue.showDialog(this.status+' - Client Error.');
+                        }
+                    } else {
+                        vue.showDialog(this.status+' - Client Error.');
+                    }
+                },
+                onDisconnected:function() {
+                    vue.showDialog('Check your internet connection and try again.');
+                }
+            }).send();
+        },
         dialogClosed:function() {
             this.dialog.show = false; 
         },
@@ -23,11 +87,62 @@ var app = new Vue({
             this.dialog.message = message;
             this.dialog.show = true;
         },
+        logout:function() {
+            var vue = this;
+            new AJAXRequest({
+                method:'post',
+                url:'cron/apis/logout',
+                params:{
+                    password:vue.password
+                },
+                beforeAjax:function() {
+                    vue.loading = true;
+                },
+                afterAjax:function() {
+                    vue.loading = false;
+                },
+                onSuccess:function() {
+                    if (this.jsonResponse) {
+                        window.location.href = 'cron/login';
+                    } else {
+                        vue.showDialog('Something went wrong. Try again.');
+                    }
+                },
+                onServerErr:function() {
+                    if (this.jsonResponse) {
+                        if (this.jsonResponse.message) {
+                            vue.showDialog(this.jsonResponse.message);
+                        } else {
+                            vue.showDialog(this.status+' - Server Error.');
+                        }
+                    } else {
+                        vue.showDialog(this.status+' - Server Error.');
+                    }
+                },
+                onClientErr:function() {
+                    if (this.jsonResponse) {
+                        if (this.jsonResponse.message) {
+                            vue.showDialog(this.jsonResponse.message);
+                        } else {
+                            vue.showDialog(this.status+' - Client Error.');
+                        }
+                    } else {
+                        vue.showDialog(this.status+' - Client Error.');
+                    }
+                },
+                onDisconnected:function() {
+                    vue.showDialog('Check your internet connection and try again.');
+                }
+            }).send();
+        },
         login:function() {
             var vue = this;
             new AJAXRequest({
                 method:'post',
                 url:'cron/apis/login',
+                params:{
+                    password:vue.password
+                },
                 beforeAjax:function() {
                     vue.loading = true;
                 },
