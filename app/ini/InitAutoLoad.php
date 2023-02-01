d0da5ae313cbc4510ce8ff3e359f734b8c19cb97
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2023-02-02 01:51:46 +0300

    Update InitAutoLoad.php

--- app/ini/InitAutoLoad.php
+++ app/ini/InitAutoLoad.php
@@ -2,26 +2,13 @@
 
 namespace app\ini;
 
-use webfiori\framework\AutoLoader;
-/**
- * A class that has one method to initialize user-defined autoload directories.
- *
- * @author Ibrahim
- * @version 1.0
- */
 class InitAutoLoad {
     /**
-     * Add user-defined directories to the set of directories at which the framework 
-     * will search for classes.
-     * The developer can use the method AutoLoader::newSearchFolder() to add 
-     * new search directory. Note that the developer does not have to 
-     * register the folder 'vendor' as it will be auto-registered.
+     * Add user-defined directories to the set of directories at which the framework will search for classes.
+     * 
+     * @since 1.0
      */
     public static function init() {
-        $AU = AutoLoader::get();
-
-        //this is a sample code that shows how to add folders.
-        $AU->newSearchFolder('my-system');
-        $AU->newSearchFolder('my-entities', false);
+            
     }
 }
