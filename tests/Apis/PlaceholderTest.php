278732238307f04170b13a2b11b1880867b3b5fa
Ibrahim BinAlshikh <ibinshikh@hotmail.com>
2026-04-09 00:46:40 +0300

    test: Updated Sample Tests and Bootstrap

---
+++ tests/Apis/PlaceholderTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace Tests\App\Apis;
+
+use PHPUnit\Framework\TestCase;
+
+class PlaceholderTest extends TestCase {
+
+    protected function setUp(): void {
+        parent::setUp();
+        
+    }
+
+    public function testTrue(): void {
+        $this->assertTrue(true);
+    }
+
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,124 +1,64 @@
 <?php
 
-//Bootstrap file which is used to boot testing process.
-use webfiori\framework\autoload\ClassLoader;
-use webfiori\framework\App;
+$root = dirname(__DIR__);
+fwrite(STDOUT, "Root: ".$root."\n");
+require $root . '/vendor/autoload.php';
 
-$DS = DIRECTORY_SEPARATOR;
+use WebFiori\Framework\App;
 
-//TODO: Set the name of JSON configuration file to use inrunning test cases.
-define('JSON_CONFIG','app-config-testing.json');
-
-
-//TODO: Set application directory name. Update as needed.
-define('APP_DIR', 'app');
-
-//This constant is used to indicate that application is in testing env.
-//Leave as is
-define('UNIT_TESTING', true);
-
-$Root = substr(__DIR__, 0, strlen(__DIR__) - strlen('tests'));
-
-//an array that contains possible locations at which 
-//WebFiori Framework might exist.
-//Add and remove directories as needed.
-$WebFioriFrameworkDirs = [
-    $Root.$DS.'webfiori',
-    $Root.$DS.'vendor'.$DS.'webfiori'.$DS.'framework'.$DS.'webfiori'
-];
-
-//Printing informative messages in the terminal
-fprintf(STDOUT, "Bootstrap Path: '".__DIR__."'\n");
-fprintf(STDOUT, "Root Path: '".$Root."'\n");
-fprintf(STDOUT,'Include Path: \''.get_include_path().'\''."\n");
-fprintf(STDOUT,"Tryning to load the class 'ClassLoader'...\n");
-$isAutoloaderLoaded = false;
-
-
-if (explode($DS, __DIR__)[0] == 'home' || explode($DS, __DIR__)[1] == 'home') {
-    fprintf(STDOUT,"Run Environment: Linux.\n");
-
-    foreach ($WebFioriFrameworkDirs as $dir) {
-        //linux 
-        $file = $DS.$dir.$DS.'framework'.$DS.'autoload'.$DS.'ClassLoader.php';
-        fprintf(STDOUT,"Checking if file '$file' is exist...\n");
-
-        if (file_exists($file)) {
-            require_once $file;
-            $isAutoloaderLoaded = true;
-            break;
-        }
-    }
-} else {
-    fprintf(STDOUT,"Run Environment: Other.\n");
-
-    foreach ($WebFioriFrameworkDirs as $dir) {
-        //other
-        $file = $dir.$DS.'framework'.$DS.'autoload'.$DS.'ClassLoader.php';
-        fprintf(STDOUT,"Checking if file '$file' is exist...\n");
-
-        if (file_exists($file)) {
-            require_once $file;
-            $isAutoloaderLoaded = true;
-            break;
-        }
-    }
+fwrite(STDOUT, "Initializing App...\n");
+try {
+    App::initiate('App', 'public', $root . '/public');
+    App::start();
+} catch (\Throwable $e) {
+    fwrite(STDOUT, "Error During Initialization: " . $e->getMessage() . "\n");
+    exit(1);
 }
+fwrite(STDOUT, "Done\n");
+fwrite(STDOUT, "----------------------------------------------\n");
 
-if ($isAutoloaderLoaded === false) {
-    fprintf(STDERR, "Error: Unable to find the class 'ClassLoader'.\n");
-    exit(-1);
-} else {
-    fprintf(STDOUT,"Class 'ClassLoader' successfully loaded.\n");
-}
-fprintf(STDOUT,"Initializing autoload directories...\n");
-ClassLoader::get([
-    'search-folders' => [
-        'tests',
-        'webfiori',
-        'vendor',
-        APP_DIR,
-    ],
-    'define-root' => true,
-    'root' => $Root,
-    'on-load-failure' => 'do-nothing'
-]);
-fprintf(STDOUT,'Autoloader Initialized.'."\n");
+//TODO: Add Database Connections and run migrations, or remove this block
 
-fprintf(STDOUT,"Initializing application...\n");
-define('APP_PATH', ClassLoader::get()->root().$DS.APP_DIR.$DS);
-fprintf(STDOUT,'App Path: '.APP_PATH."\n");
-$driver = "\\webfiori\\framework\\config\\JsonDriver";
-fprintf(STDOUT,"Setting application configuration driver to '$driver'\n");
-App::setConfigDriver($driver);
-$configFileName = 'app-config-testing.json';
-fprintf(STDOUT,"Setting application configuration file to '$configFileName'\n");
-App::initiate(APP_DIR);
-App::getConfig()->setConfigFileName($configFileName);
-App::getConfig()->initialize();
-App::start();
-fprintf(STDOUT,'Done.'."\n");
-fprintf(STDOUT,'Root Directory: \''.ClassLoader::get()->root().'\'.'."\n");
-define('TESTS_PATH', ClassLoader::get()->root().$DS.'tests');
-fprintf(STDOUT,'Stored Connections:'."\n");
-if (count(App::getConfig()->getDBConnections()) != 0) {
-    foreach (App::getConfig()->getDBConnections() as $conn) {
-        fprintf(STDOUT, $conn->getName()."\n");
-    }
-} else {
-    fprintf(STDOUT, "<<NO DATABSE CONNECTIONS>>\n");
-}
-fprintf(STDOUT, "Registering shutdown function...\n");
-register_shutdown_function(function()
-{
-   //TODO: Run extra code after tests completion. 
-    fprintf(STDOUT, "Testing Finished.\n");
-});
-fprintf(STDOUT, "Registering shutdown function completed.\n");
-require_once 'query-runner.php';
+//fwrite(STDOUT, "Adding Database Connection...\n");
+//
+// $exitCode = App::getRunner()->setArgsVector([
+//     'webfiori',
+//     'add:db-connection',
+//     '--db-type' => 'mysql',
+//     '--host' => getenv('DB_HOST'),
+//     '--port' => '3306',
+//     '--user' => getenv('DB_USER'),
+//     '--password' => getenv('DB_PASS'),
+//     '--database' => 'db_name',
+//     '--name' => 'my-connection',
+//     '--no-check'
+// ])->start();
 
-//TODO: Include your own custom bootstrap scripts here.
-//require_once 'my-script.php';
+// if ($exitCode != 0) {
+//     fwrite(STDOUT, "Error During Initialization. Tests Will not Execute\n");
+//     fwrite(STDOUT, "----------------------------------------------\n");
+//     exit($exitCode);
+// }
+// fwrite(STDOUT, "Done\n");
+// fwrite(STDOUT, "----------------------------------------------\n");
 
-fprintf(STDOUT, "Done\n");
-fprintf(STDOUT,"Starting to run tests...\n");
+// fwrite(STDOUT, "Applying Migrations...\n");
+// $exitCode = App::getRunner()->setArgsVector([
+//     'webfiori',
+//     'migrations:fresh',
+//     '--connection' => 'my-connection',
+//     '--env' => 'dev'
+// ])->start();
+// if ($exitCode != 0) {
+//     fwrite(STDOUT, "Error During Initialization. Tests Will not Execute\n");
+//     fwrite(STDOUT, "----------------------------------------------\n");
+//     exit($exitCode);
+// }
+
+//TODO: Routines after testing. Or Remove
+// register_shutdown_function(function () {
+    
+// });
+// fwrite(STDOUT, "----------------------------------------------\n");
+fwrite(STDOUT, "Bootstrapping Done\n");
+fwrite(STDOUT, "----------------------------------------------\n");
